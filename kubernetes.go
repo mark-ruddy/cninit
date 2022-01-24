99f1f1927fcd06aea4d3aef5e46be8b69c48384e
mark-ruddy <1markruddy@gmail.com>
2022-01-24 16:27:08 +0000

    revert to non-broken k8s versions

--- kubernetes.go
+++ kubernetes.go
@@ -1,10 +1,7 @@
 package cninit
 
 import (
-	"context"
 	"fmt"
-	"k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
